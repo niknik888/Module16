8998ce9f7ff7717ea2569e9032b3892f40a9b54d
nvnik <nvnik@NIK>
2024-02-06 20:31:37 +0300

    Добавьте файлы проекта.

---
+++ Module16.Tests/CalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Module16.Tests
+{
+    class CalculatorTests
+    {
+        [Test]
+        public void MultiplicationMustReturnValueNotNull()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual(8, calculator.Multiplication(2, 4));
+
+        }
+
+        [Test]
+        public void SubstractionMustReturnRightValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(100, 10) == 90);
+
+        }
+        
+        [Test]
+        public void DivisionMustReturnRightValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(100, 10) == 10);
+
+        }
+
+    }
+}
---
+++ Module16/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module16
+{
+    public class Calculator
+    {
+        public int Multiplication(int a, int b)
+        {
+            return a * b; 
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+        
+    }
+}
